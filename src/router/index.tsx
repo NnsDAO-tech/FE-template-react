d820d1f3df3a9c1afabef9a6f7543ef925cc2d38
neptune <103205025+neptune0xf@users.noreply.github.com>
2022-11-03 10:19:39 +0800

    react router config

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,14 +1,7 @@
-import React from 'react';
-import type { RouteObject } from 'react-router-dom';
-import { useRoutes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from '../pages/Home';
-const Demo = React.lazy(() => import('../components/Demo'));
-const Swap = React.lazy(() => import('../pages/swap/Index'));
-const Pool = React.lazy(() => import('../pages/pool/Index'));
-const Aboutnswap = React.lazy(() => import('../pages/aboutnswap/Index'));
-const Vote = React.lazy(() => import('../pages/vote/Index'));
 
-const routes: RouteObject[] = [
+const routes = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
@@ -25,88 +18,8 @@ const routes: RouteObject[] = [
       // { path: "*", element: <NoMatch /> },
     ],
   },
-  {
-    path: '/demo',
-    element: <Demo />,
-    children: [
-      // { index: true, element: <Home /> },
-      // {
-      //   path: "/courses",
-      //   element: <Courses />,
-      //   children: [
-      //     { index: true, element: <CoursesIndex /> },
-      //     { path: "/courses/:id", element: <Course /> },
-      //   ],
-      // },
-      // { path: "*", element: <NoMatch /> },
-    ],
-  },
-  {
-    path: '/swap',
-    element: <Swap></Swap>,
-    children: [
-      // { index: true, element: <Home /> },
-      // {
-      //   path: "/courses",
-      //   element: <Courses />,
-      //   children: [
-      //     { index: true, element: <CoursesIndex /> },
-      //     { path: "/courses/:id", element: <Course /> },
-      //   ],
-      // },
-      // { path: "*", element: <NoMatch /> },
-    ],
-  },
-  {
-    path: '/pool',
-    element: <Pool></Pool>,
-    children: [
-      // { index: true, element: <Home /> },
-      // {
-      //   path: "/courses",
-      //   element: <Courses />,
-      //   children: [
-      //     { index: true, element: <CoursesIndex /> },
-      //     { path: "/courses/:id", element: <Course /> },
-      //   ],
-      // },
-      // { path: "*", element: <NoMatch /> },
-    ],
-  },
-  {
-    path: '/aboutnswap',
-    element: <Aboutnswap></Aboutnswap>,
-    children: [
-      // { index: true, element: <Home /> },
-      // {
-      //   path: "/courses",
-      //   element: <Courses />,
-      //   children: [
-      //     { index: true, element: <CoursesIndex /> },
-      //     { path: "/courses/:id", element: <Course /> },
-      //   ],
-      // },
-      // { path: "*", element: <NoMatch /> },
-    ],
-  },
-  {
-    path: '/vote',
-    element: <Vote></Vote>,
-    children: [
-      // { index: true, element: <Home /> },
-      // {
-      //   path: "/courses",
-      //   element: <Courses />,
-      //   children: [
-      //     { index: true, element: <CoursesIndex /> },
-      //     { path: "/courses/:id", element: <Course /> },
-      //   ],
-      // },
-      // { path: "*", element: <NoMatch /> },
-    ],
-  },
-];
+]);
 export default function AppRouter() {
   window.scroll(0, 0);
-  return useRoutes(routes);
+  <RouterProvider router={routes}></RouterProvider>;
 }
